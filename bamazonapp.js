c3c17003f19af579117ff4e5895f5e39f6f844b0
Kyle LaMont <kylerlamont@gmail.com>
2019-11-13 11:08:54 -0800

    added purchase prompting to select item ID for customers and quantity for them. Verified script works as intended.

--- bamazonapp.js
+++ bamazonapp.js
@@ -13,4 +13,46 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id" + connection.threadId);
+    displayProducts();
 });
+
+function displayProducts() {
+    var query = "Select * FROM products";
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+        var displayTable = new Table({
+            head: ["Item ID", "Product Name", "Catergory", "Price", "Quantity"],
+            colWidths: [10, 25, 25, 10, 14]
+        });
+        for (var i = 0; i < res.length; i++) {
+            displayTable.push(
+                [res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
+            );
+        }
+        console.log(displayTable.toString());
+        purchasePrompt();
+    });
+}
+
+function purchasePrompt() {
+    inquirer.prompt([
+        {
+            name: "ID",
+            type: "input",
+            message: "Please enter Item ID you like to purhcase.",
+            filter: Number
+        },
+        {
+            name: "Quantity",
+            type: "input",
+            message: "How many items do you wish to purchase?",
+            filter: Number
+        },
+
+    ]).then(function (answers) {
+        var quantityNeeded = answers.Quantity;
+        var IDrequested = answers.ID;
+        //purchaseOrder(IDrequested, quantityNeeded);
+        console.log(quantityNeeded + IDrequested);
+    });
+};
